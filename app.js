87677ca1be9aba451e490697e77520873de6f35c
Jonathan Reeves <jonathanreeves@jonathans-mbp.attlocal.net>
2018-10-11 20:54:43 -0500

    Updated Procfile

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get('/receive', (req, res) => {
     res.send('Hi, this is the TwilioBot listening endpoint!');
 });
 
+app.post('/receive', (req, res) => {
+    let twiml = new twilio.TwimlResponse();
+    var feedback = BotBrains(req.body);
+    twiml.message(feedback)
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
 app.listen(process.env.port, () => {
     console.log('TwilioBot listening on port 8080.');
 })
@@ -18,8 +27,8 @@ var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
 client.messages.create({
-    body: 'Hello, I am here to take over your phone, -TwilioSmsBot',
-    to: '+16822131666',
+    body: 'Hello, I am here to take over your phone, -NodeBot',
+    to: '+12094041855',
     from: '+18329349236'
 })
 .then((message) => console.log(message.sid));
